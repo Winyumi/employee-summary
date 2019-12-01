171f1d7c083ca1e5b5eb6a64af2b7a76c1f2a4f6
Winyumi <winguy2003@hotmail.com>
2019-11-30 23:44:37 -0500

    restructure validation, fix id validation

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-const required = input => input !== '' ? true : "This field is required.";
-const validateName = input => input !== '' ? true : "Please enter a name.";
-const validateId = input => !Number.isInteger(input) && input > 0 ? true : "Please enter a positive whole number.";
-const validateEmail = input => input.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+\@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi) ? true : "Please enter a valid email address.";
+const validate = {
+    required: input => input !== '' ? true : "This field is required.",
+    name: input => input !== '' ? true : "Please enter a name.",
+    id: input => Number.isInteger(Number(input)) && Number(input) > 0 ? true : "Please enter a positive whole number.",
+    email: input => input.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+\@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi) ? true : "Please enter a valid email address."
+}
 
 const questions = {
     type: function() {
@@ -34,20 +36,20 @@ const questions = {
 let employees = [];
 
 async function addRole(member) {
-    let { name } = await inquirer.prompt(questions.item(member, "name", "full name", validateName));
-    let { id } = await inquirer.prompt(questions.item(member, "id", "ID number", validateId));
-    let { email } = await inquirer.prompt(questions.item(member, "email", "email address", validateEmail));
+    let { name } = await inquirer.prompt(questions.item(member, "name", "full name", validate.name));
+    let { id } = await inquirer.prompt(questions.item(member, "id", "ID number", validate.id));
+    let { email } = await inquirer.prompt(questions.item(member, "email", "email address", validate.email));
     switch (member) {
         case "Manager":
-            let { phone } = await inquirer.prompt(questions.item(member, "phone", "office phone number", required));
+            let { phone } = await inquirer.prompt(questions.item(member, "phone", "office phone number", validate.required));
             employees.push(new Manager(name, id, email, phone));
             break;
         case "Engineer":
-            let { github } = await inquirer.prompt(questions.item(member, "github", "GitHub username", required));
+            let { github } = await inquirer.prompt(questions.item(member, "github", "GitHub username", validate.required));
             employees.push(new Engineer(name, id, email, github));
             break;
         case "Intern":
-            let { school } = await inquirer.prompt(questions.item(member, "school", "school", required));
+            let { school } = await inquirer.prompt(questions.item(member, "school", "school", validate.required));
             employees.push(new Intern(name, id, email, school));
             break;
     }
