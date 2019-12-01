d7875bfc3790480c7f079e928b0670ad403728cf
Winyumi <winguy2003@hotmail.com>
2019-11-30 20:55:44 -0500

    set questions to be dynamic

--- app.js
+++ app.js
@@ -7,46 +7,65 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-
-function validate(a) {
-    return a !== '';
+function validate(input) {
+    return input !== '';
 }
 
-const questions = [
-    {
-        message: "Which type of team member would you like to add?",
-        type: "list",
-        name: "type",
-        choices: ["Engineer", "Intern", "I don't want to add anymore team members"]
+const questions = {
+    type: function() {
+        return {
+            message: "Which type of team member would you like to add?",
+            type: "list",
+            name: "member",
+            choices: ["Engineer", "Intern", "I don't want to add anymore team members"]
+        }
     },
-    {
-        message: `What is your ${type.toLowerCase()}'s name?`,
-        type: "input",
-        name: "name",
-        validate: validate
-    },
-    {
-        message: `What is your ${type.toLowerCase()}'s id?`,
-        type: "input",
-        name: "id",
-        validate: validate
-    },
-    {
-        message: `What is your ${type.toLowerCase()}'s name?`,
-        type: "input",
-        name: "name",
-        validate: validate
-    },
-    {
-        message: `What is your ${type.toLowerCase()}'s name?`,
-        type: "input",
-        name: "name",
-        validate: validate
-    },
-    {
-        message: `What is your ${type.toLowerCase()}'s name?`,
-        type: "input",
-        name: "name",
-        validate: validate
+    item: function(member, variable, item = variable) {
+        return {
+            message: `What is your ${member.toLowerCase()}'s ${item}?`,
+            type: "input",
+            name: variable,
+            validate: validate
+        }
     }
-];
+};
+
+async function init() {
+
+    let manager = true;
+    let exit = "I don't want to add anymore team members";
+    while (member != exit) {
+        if (manager) {
+            var member = "Manager";
+        } else {
+            var { member } = await inquirer.prompt(questions.type());
+            if (member === exit) {
+                return;
+            }
+        }
+        var { name } = await inquirer.prompt(questions.item(member, "name", "full name"));
+        var { id } = await inquirer.prompt(questions.item(member, "id", "ID number"));
+        var { email } = await inquirer.prompt(questions.item(member, "email", "email address"));
+        switch (member) {
+            case "Manager":
+                var { phone } = await inquirer.prompt(questions.item(member, "phone", "phone number"));
+                var employee = new Manager(id, name, email, phone);
+                break;
+            case "Engineer":
+                var { github } = await inquirer.prompt(questions.item(member, "github", "GitHub username"));
+                var employee = new Engineer(id, name, email, github);
+                break;
+            case "Intern":
+                var { school } = await inquirer.prompt(questions.item(member, "school"));
+                var employee = new Intern(id, name, email, school);
+                break;
+        }
+        manager = false;
+    }
+
+
+    console.log(employee);
+
+}
+
+init();
