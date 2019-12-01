70123364ec9f4b3d03d617dc1956097c7c3e183d
Winyumi <winguy2003@hotmail.com>
2019-11-30 23:24:30 -0500

    expand validation

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-function validate(input) {
-    return input !== '';
-}
+const required = input => input !== '' ? true : "This field is required.";
+const validateName = input => input !== '' ? true : "Please enter a name.";
+const validateId = input => !Number.isInteger(input) && input > 0 ? true : "Please enter a positive whole number.";
+const validateEmail = input => input.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+\@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi) ? true : "Please enter a valid email address.";
 
 const questions = {
     type: function() {
@@ -20,7 +21,7 @@ const questions = {
             choices: ["Engineer", "Intern", "I don't want to add anymore team members"]
         }
     },
-    item: function(member, variable, item = variable) {
+    item: function(member, variable, item = variable, validate) {
         return {
             message: `What is your ${member.toLowerCase()}'s ${item}?`,
             type: "input",
@@ -33,20 +34,20 @@ const questions = {
 let employees = [];
 
 async function addRole(member) {
-    let { name } = await inquirer.prompt(questions.item(member, "name", "full name"));
-    let { id } = await inquirer.prompt(questions.item(member, "id", "ID number"));
-    let { email } = await inquirer.prompt(questions.item(member, "email", "email address"));
+    let { name } = await inquirer.prompt(questions.item(member, "name", "full name", validateName));
+    let { id } = await inquirer.prompt(questions.item(member, "id", "ID number", validateId));
+    let { email } = await inquirer.prompt(questions.item(member, "email", "email address", validateEmail));
     switch (member) {
         case "Manager":
-            let { phone } = await inquirer.prompt(questions.item(member, "phone", "office phone number"));
+            let { phone } = await inquirer.prompt(questions.item(member, "phone", "office phone number", required));
             employees.push(new Manager(name, id, email, phone));
             break;
         case "Engineer":
-            let { github } = await inquirer.prompt(questions.item(member, "github", "GitHub username"));
+            let { github } = await inquirer.prompt(questions.item(member, "github", "GitHub username", required));
             employees.push(new Engineer(name, id, email, github));
             break;
         case "Intern":
-            let { school } = await inquirer.prompt(questions.item(member, "school"));
+            let { school } = await inquirer.prompt(questions.item(member, "school", "school", required));
             employees.push(new Intern(name, id, email, school));
             break;
     }
